e650b68cd232452bc79ab067e28572676ed24e8f
Niktae <139506807+Niktae@users.noreply.github.com>
2025-04-02 01:07:11 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (80.37%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,15 +1,23 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        //binary search approach tc-0(log n)
         int n=nums.length;
-        int a=0;
         if(n==1) return nums[0];
         if(nums[0]!=nums[1]) return nums[0];
-        if(nums[n-1]!=nums[n-2])return nums[n-1];
-        for(int i=1;i<nums.length-1;i++){
-            if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]){
-                  a=nums[i];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+        int low=1,high=n-2;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
+                return nums[mid];
+            }
+            if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1])){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
             }
         }
-        return a ;
+        return -1;
     }
 }
