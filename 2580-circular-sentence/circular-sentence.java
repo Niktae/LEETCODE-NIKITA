370513abacc4ba1fd43c75f361ff73c5532f79dc
Niktae <139506807+Niktae@users.noreply.github.com>
2025-04-14 03:33:06 +0530

    Time: 1 ms (90.82%) | Memory: 42.2 MB (52.64%) - LeetSync

--- 2580-circular-sentence/circular-sentence.java
+++ 2580-circular-sentence/circular-sentence.java
@@ -1,20 +1,22 @@
 class Solution {
     public boolean isCircularSentence(String sentence) {
-        int n=sentence.length();
-        if(sentence.charAt(0)!=sentence.charAt(n-1)){
+        int n = sentence.length();
+        if (sentence.charAt(0) != sentence.charAt(n - 1)) {
             return false;
         }
-        int c=sentence.indexOf(" ");
-        if(c==-1) return true;
-        while(c!=-1){
-            if(sentence.charAt(c-1)!=sentence.charAt(c+1)){
-                return false;
+        for (int i = 0; i < n; i++) {
+            if (sentence.charAt(i) == ' ') {
+                char lastChar = sentence.charAt(i - 1);
+                char nextFirstChar = sentence.charAt(i + 1);
+                if (lastChar != nextFirstChar) {
+                    return false;
+                }
             }
-            c=sentence.indexOf(" ",c+1);
         }
         return true;
     }
 }
+
 
         
         
