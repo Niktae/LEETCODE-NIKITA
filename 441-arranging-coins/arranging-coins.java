15afd5510aa3daffd9c160a5799161fff16fc1b9
Niktae <139506807+Niktae@users.noreply.github.com>
2025-05-08 16:24:43 +0530

    Time: 9 ms (12.54%) | Memory: 40.6 MB (96.99%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,15 +1,11 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int cnt=0; 
-        if (n==1) return 1;
-        if(n==0) return 0;
-        int i=1;
-        while(i<=n){
+        int cnt=0;
+        for(int i=1;i<=n;i++){
             if(i<=n){
                 cnt++;
                 n-=i;
-                i++;
-            }
+            } 
         }
         return cnt;
     }
