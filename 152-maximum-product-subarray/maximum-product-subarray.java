f42cd58fcaa6d6cf25c3f92003a41fdb58b17eb1
Niktae <139506807+Niktae@users.noreply.github.com>
2025-05-10 16:56:41 +0530

    Time: 1 ms (90.36%) | Memory: 44.9 MB (72.76%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -3,14 +3,14 @@ class Solution {
         //better or brute force approach
         int n= nums.length;
         int max=Integer.MIN_VALUE;
+        int prefix=1,suffix=1;
         for(int i=0;i<n;i++){
-            int prod=1;
-            for(int j=i;j<n;j++){
-                prod*=nums[j];
-                max=Math.max(max,prod);
-            }
+            if(prefix==0) prefix=1;
+            if(suffix==0) suffix=1;
+                prefix*=nums[i];
+                suffix*=nums[n-i-1];
+                max=Math.max(max,Math.max(prefix,suffix));
         }
         return max;
-        
     }
 }
