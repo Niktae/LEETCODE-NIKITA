0d107017075e7ab61d8b8a5abc91011e262eca6d
Niktae <139506807+Niktae@users.noreply.github.com>
2025-09-08 23:53:46 +0530

    Time: 2 ms (44.21%) | Memory: 59.1 MB (52.21%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int min=prices[0];
+        int profit=0;
+        for(int i=1;i<prices.Length;i++){
+            if(prices[i]<min){
+                min=prices[i];
+            }
+            else{
+                profit=Math.Max(profit,prices[i]-min);
+            }
+        }
+        return profit;
+        
+    }
+}
