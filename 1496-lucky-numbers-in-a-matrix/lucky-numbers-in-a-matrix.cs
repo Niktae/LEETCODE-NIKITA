ba1d8f0d89dc14bf7dc5d3528c754cadd433c2d2
Niktae <139506807+Niktae@users.noreply.github.com>
2025-09-17 02:35:19 +0530

    Time: 5 ms (61.22%) | Memory: 51.7 MB (44.90%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cs
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cs
@@ -4,7 +4,7 @@ public class Solution {
         int n=matrix[0].Length;
         int[] rowmin=new int[m];
         for(int i=0;i<m;i++){
-            int min=matrix[i][0];
+            int min=int.MaxValue;
             for(int j=0;j<n;j++){
                 if(matrix[i][j]<min) min=matrix[i][j];
             }
@@ -13,7 +13,7 @@ public class Solution {
         //for max column
         int[] colmax=new int[n];
         for(int j=0;j<n;j++){
-            int max=matrix[0][j];
+            int max=int.MinValue;
             for(int i=0;i<m;i++){
                 if(matrix[i][j]>max) max=matrix[i][j];
             }
