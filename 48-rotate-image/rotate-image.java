fe82c569c39a166487b8fc5df3b251e968fef676
Niktae <139506807+Niktae@users.noreply.github.com>
2025-12-06 01:16:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (21.96%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -11,7 +11,7 @@ class Solution {
             }
         }
         for(int i=0;i<n;i++){
-            for(int j=0;j<n/2;j++){
+            for(int j=0;j<m/2;j++){
                 int temp=matrix[i][j];
                 matrix[i][j]=matrix[i][n-1-j];
                 matrix[i][n-1-j]=temp;
