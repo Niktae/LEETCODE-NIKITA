c92df8dc01707ecbaf47626bcc786bba91a9d860
Niktae <139506807+Niktae@users.noreply.github.com>
2025-12-10 02:35:00 +0530

    Time: 430 ms (5.51%) | Memory: 189.9 MB (42.52%) - LeetSync

---
+++ 3885-count-special-triplets/count-special-triplets.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int specialTriplets(int[] nums) {
+        final long MOD=1000000007L;
+        long ans=0;
+        Map<Integer,Long>left=new HashMap<>();
+        Map<Integer,Long>right=new HashMap<>();
+        for(int num:nums){
+            right.put(num,right.getOrDefault(num,0L)+1);
+        }
+        //remove j from right as j is like middle element
+        for(int j=0;j<nums.length;j++){
+            right.put(nums[j],right.get(nums[j])-1);
+            if(right.get(nums[j])==0) {
+                right.remove(nums[j]);
+            }
+            int target=2*nums[j];
+            long lcnt=left.getOrDefault(target,0L);
+            long rcnt=right.getOrDefault(target,0L);
+            ans+=((lcnt*rcnt)%MOD)%MOD;
+            //Now put j in left
+            left.put(nums[j],left.getOrDefault(nums[j],0L)+1);
+        }
+        return (int)((ans%MOD)%MOD);
+
+    }
+}
