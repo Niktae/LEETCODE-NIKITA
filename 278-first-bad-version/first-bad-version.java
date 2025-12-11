3df1ca235ff9771dd6c13468ef815f4dc4a81641
Niktae <139506807+Niktae@users.noreply.github.com>
2025-12-12 00:56:05 +0530

    Time: 13 ms (99.83%) | Memory: 42.1 MB (15.98%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -7,6 +7,8 @@ public class Solution extends VersionControl {
         int low=1,high=n;
         while(low<=high){
             int mid=low+(high-low)/2;
+            //int mid=(low+high)/2;//This will give tle because  of integ overflow 
+            //some times doing low + high may add to a number larger than the int bits can store therefore high - low can be lower then that so yes although there final result is same to major difference is how they reach to that part
             if(isBadVersion(mid)){
                 cnt=mid;
                 high=mid-1;               
